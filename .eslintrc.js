6418352e77e4ec88b68f5bbae422376ce48ecaf8
Dilip Kr. Shukla <contact@fusionstrings.com>
2018-03-20 10:06:09 +0100

    use react preset instead of plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true,
-        "worker": true,
-        "jest": true,
-        "serviceworker": true,
-        "nashorn": true
-    },
-    "extends": ["eslint:recommended", "plugin:prettier/recommended"],
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "func-style": "off"
-    }
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true,
+		node: true,
+		worker: true,
+		jest: true,
+		serviceworker: true,
+		nashorn: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:prettier/recommended',
+		'plugin:react/recommended'
+	],
+	parser: 'babel-eslint',
+	parserOptions: {
+		ecmaVersion: 2018,
+		ecmaFeatures: {
+			jsx: true
+		},
+		sourceType: 'module'
+	},
+	rules: {
+		'func-style': 'off'
+	}
 };
